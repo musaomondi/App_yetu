58dbcbe85d9e6456d3f471cc752a3e23d8afcba9
marcoderoh <marcoderoh@gmail.com>
2016-06-22 17:03:03 +0300

    added user tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,11 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+
   test "should get new" do
-    get users_new_url
+    get new_user_path
     assert_response :success
   end
+
 
 end
